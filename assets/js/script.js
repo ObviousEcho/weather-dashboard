a77caaf8e3f6cbfd6cda3a0678f351b9ac01113e
Adia & Indy <107782632+ObviousEcho@users.noreply.github.com>
2022-08-20 16:29:46 -0600

    psuedo code script js

---
+++ assets/js/script.js
@@ -0,0 +1,29 @@
+
+
+// write a function to make an API call for a specific city, which returns data
+// function should submit city input from form into API endpoint 
+// function should add city to search history
+// ===========================================================================
+
+
+// write a function which dislpays current conditoins for that city
+// dynamically display city name, date, an icon, temp, humidity, wind and uv index
+// uv index to be color coded
+// ===========================================================================
+
+
+// write function which dynamically displays the 5 day forcast, date, an icon, 
+// temp, wind speed, and humidity
+// ===========================================================================
+
+
+// write function which displays current and 5 day forcast for cities from search history
+// ===========================================================================
+
+
+// write function which dynamically displays search history
+// ===========================================================================
+
+
+// create an event listener for submit button
+// ===========================================================================
