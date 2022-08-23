e992264124657e0a6fd7c3c86b4b6a5f064ebaee
Adia & Indy <107782632+ObviousEcho@users.noreply.github.com>
2022-08-22 23:20:26 -0600

    updating readme, create dynamic 5 day forecast title, add comments in js file

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # weather-dashboard
+
+Weather Dashboard is a weather forecast website which displays current, as well as a five-day forecast for
+any city searched through the search bar. Search criteria displays results and also stores the city searched into a search history. Upon either searching a city, or selecting a city from the search history, any results which were previously displayed, are replaced by results from the current selection.
+
+## API's
+
+This project utilized API's three times over. DOM API's, third party API's, as well as a server side API. A minimal amount of hard coded HTML and CSS allowed me to dynamically create needed content with Javascript to interact with the DOM. Usage of a CSS library was incorporated and an open source server side API for the weather data.
+
+## Technologies Used
+
+- Web API methods
+- Bootstrap
+- Openweather API
+- Javascript
+- CSS
+- HTML
+
+---
+
+## Challenges
+
+This was an enjoyable project, from the get go I felt as if I had a sense of direction for how to proceed. It was however not without it's challenges. Having to rely on a seperate entities documentation to accomplish your end results can certainly put you to task. The Openweather docs are fairly well written, after significant time researching the docs, I did have to resort to googling a couple issues to see how other developers had already resolved those same issues. For example, I had to search for the proper image url path that Openweather uses to insert icon codes into so that I could display icons representing various weather conditions.
+
+## Conclusions
+
+
--- assets/js/script.js
+++ assets/js/script.js
@@ -6,6 +6,7 @@ var searchHistory = [];
 var forecastData = [];
 var ul = document.getElementById("list");
 
+// get and display search history to DOM
 getLocalStorage();
 appendHistory();
 
@@ -18,6 +19,7 @@ function getApi(event) {
   var responseData = [];
   var inputField = document.getElementById("input-field");
   var cityInput = inputField.value;
+  // url for current weather conditions
   var requestUrl =
     "https://api.openweathermap.org/data/2.5/weather?q=" +
     cityInput +
@@ -27,6 +29,7 @@ function getApi(event) {
 
   fetch(requestUrl)
     .then(function (response) {
+      // if response other than 200, display error page
       if (response.status !== 200) {
         document.location.replace(redirectUrl);
       } else {
@@ -35,24 +38,31 @@ function getApi(event) {
     })
     .then(function (data) {
       responseData.push(data);
+      // function call stores city/response to local storage
       sendToLocalStorage(responseData);
+      // function call deletes current weather info and appends new info when a new city is selected
       deleteThenAppendHistory(responseData);
 
+      // var's to pass into createCurrentCard function
       var city = responseData[0].name;
       var date = responseData[0].dt;
       var icon = responseData[0].weather[0].icon;
       var temp = responseData[0].main.temp;
       var wind = responseData[0].wind.speed;
       var humid = responseData[0].main.humidity;
-
+      // function dynamically creates card to display data from http request
       createCurrentCard(city, date, icon, temp, wind, humid);
     });
+  // function call makes seperate http request for 5 day forecast
   fiveDayForecast(cityInput);
+  // clears search bar after submit
   inputField.value = "";
 }
 
 // write a function witch fetches forecast data for search city
+// ====================================================================
 function fiveDayForecast(city) {
+  // url for 5 day forecast
   var forecastUrl =
     "https://api.openweathermap.org/data/2.5/forecast?q=" +
     city +
@@ -62,6 +72,7 @@ function fiveDayForecast(city) {
 
   fetch(forecastUrl)
     .then(function (response) {
+      // if response other than 200, display error page
       if (response.status !== 200) {
         document.location.replace(redirectUrl);
       } else {
@@ -69,7 +80,7 @@ function fiveDayForecast(city) {
       }
     })
     .then(function (data) {
-      // forecastData.push(data.list);
+      // parse 5 day forecast results into array
       forecastData = data.list;
       fiveDayAppend(forecastData);
     });
@@ -78,6 +89,7 @@ function fiveDayForecast(city) {
 // write a function which removes child nodes from history list, and then
 // calls function to append local storage to history list
 // this prevents prevents history list from double appending
+// ===========================================================================
 function deleteThenAppendHistory(arr) {
   while (ul.hasChildNodes()) {
     ul.removeChild(ul.firstChild);
@@ -85,11 +97,11 @@ function deleteThenAppendHistory(arr) {
   appendHistory();
 }
 
-// write a function which dislpays current conditoins for that city
-// dynamically display city name, date, an icon, temp, humidity, wind and uv index
-// uv index to be color coded
+// write a function which dislpays current conditions for that city
+// dynamically display city name, date, an icon, temp, humidity, and wind
 // ===========================================================================
 function createCurrentCard(city, date, icon, temp, wind, humid) {
+  // crate elements
   var dashboard = document.getElementById("dashboard");
   var iconImg = document.createElement("img");
   var span = document.createElement("span");
@@ -97,21 +109,22 @@ function createCurrentCard(city, date, icon, temp, wind, humid) {
   var temperature = document.createElement("p");
   var windSpeed = document.createElement("p");
   var humidity = document.createElement("p");
-
+  // convert date (unix time) from response into standard format
   var miliseconds = date * 1000;
   var dateObject = new Date(miliseconds);
   var formattedDate = dateObject.toLocaleDateString();
-
+  // base image url from openweather api to display icon codes from response
   var iconUrl = "http://openweathermap.org/img/w/" + icon + ".png";
-
+  // add border to current results card/box
   dashboard.style.border = "solid black 2px";
-
+  // remove current results card/box to prevent new searches from appending below
+  // only current selection will display
   if (dashboard !== null) {
     while (dashboard.hasChildNodes()) {
       dashboard.removeChild(dashboard.firstChild);
     }
   }
-
+  // dynamically create elements for current forecast
   iconImg.setAttribute("src", iconUrl);
   span.appendChild(iconImg);
 
@@ -128,22 +141,23 @@ function createCurrentCard(city, date, icon, temp, wind, humid) {
   dashboard.appendChild(humidity);
 }
 
-// write function which dynamically displays the 5 day forcast, date, an icon,
+// write function which dynamically parses the 5 day forcast, date, an icon,
 // temp, wind speed, and humidity
 // ===========================================================================
 function fiveDayAppend(data) {
   var fiveDayArray = [];
+  // loop through 5 day forecast response data
   for (i = 0; i < data.length; i++) {
-    // console.log(data[i]);
-
+    // convert date (unix time) from response into standard format
     var miliseconds = data[i].dt * 1000;
     var dateObject = new Date(miliseconds);
     var formattedDate = dateObject.toLocaleDateString();
+    // store response data in var's
     var icon = data[i].weather[0].icon;
     var temperature = data[i].main.temp;
     var wind = data[i].wind.speed;
     var humid = data[i].main.humidity;
-
+    // object storing response data
     var forecast = {
       date: formattedDate,
       icon: icon,
@@ -151,14 +165,17 @@ function fiveDayAppend(data) {
       wind: wind,
       humidity: humid,
     };
-
+    // store objects with desired 5 day forecast data in array, API only allows search in 3 hour
+    // segments up to five days, array contains 40 - 3 hour blocks
     fiveDayArray.push(forecast);
   }
+  // crate array with indexes to discard
   var removeValFromIndex = [
     1, 2, 3, 4, 5, 6, 7, 9, 10, 11, 12, 13, 14, 15, 17, 18, 19, 20, 21, 22, 23,
     25, 26, 27, 28, 29, 30, 31, 33, 34, 35, 36, 37, 38, 39,
   ];
-
+  // loop through discard array backwards, and remove said indexes from fiveDayArray
+  // without altering index positions in fiveDayArray
   for (var i of removeValFromIndex.reverse()) {
     fiveDayArray.splice(i, 1);
   }
@@ -166,16 +183,17 @@ function fiveDayAppend(data) {
 }
 
 // write function which appends 5 day forecast data to DOM
+// ==============================================================
 function fiveDayToDOM(data) {
   var fiveDayDiv = document.getElementById("five-day");
-  console.log(data);
-
+  // remove five day forecast so forecast from new search/selection does not append below
+  // only one five day forecast will display at a time
   if (fiveDayDiv !== null) {
     while (fiveDayDiv.hasChildNodes()) {
       fiveDayDiv.removeChild(fiveDayDiv.firstChild);
     }
   }
-
+  // loop through response data and create a display card for 5 days of weather
   for (i = 0; i < data.length; i++) {
     var iconUrl = "http://openweathermap.org/img/w/" + data[i].icon + ".png";
     var div = document.createElement("div");
@@ -200,15 +218,27 @@ function fiveDayToDOM(data) {
     div.appendChild(humid);
     fiveDayDiv.appendChild(div);
   }
+  // remove "5 Day Forecast" title to prevent it from double appending with new city search
+  var fiveDayHeading = document.getElementById("five-day-heading");
+  if (fiveDayHeading !== null) {
+    fiveDayHeading.remove();
+  }
+  // dynamically crate "5 Day Forecast" title for cards
+  var forecastDashboard = document.getElementById("dashboard");
+  var title = document.createElement("h3");
+  title.textContent = "5 Day Forecast";
+  title.setAttribute("class", "ms-4 mt-4");
+  title.setAttribute("id", "five-day-heading");
+  forecastDashboard.after(title);
 }
 
 // write function which displays current forcast for cities from search history
 // ===========================================================================
 function appendCityToCurrent(event) {
+  // event targets element clicked to display forecast from search history
   var btnClicked = event.target;
   var btnText = btnClicked.textContent;
-  console.log(btnText);
-  console.log(searchHistory);
+  // loop through search history results
   for (i = 0; i < searchHistory.length; i++) {
     var city = searchHistory[i].city;
     var date = searchHistory[i].date;
@@ -216,10 +246,13 @@ function appendCityToCurrent(event) {
     var temp = searchHistory[i].temp;
     var wind = searchHistory[i].wind;
     var humid = searchHistory[i].humidity;
+    // button clicked must match object in searchHistory array
     if (btnText === city) {
+      // pass in data to dynamically create current forecast card
       createCurrentCard(city, date, icon, temp, wind, humid);
     }
   }
+  // pass in city name from event to get 5 day forecast for city from search history
   fiveDayForecast(btnText);
 }
 
@@ -243,7 +276,6 @@ function sendToLocalStorage(arr) {
     temp: obj.main.temp,
     humidity: obj.main.humidity,
     wind: obj.wind.speed,
-    // uv: uv
   };
   searchHistory.push(cityData);
   localStorage.setItem("searchHistory", JSON.stringify(searchHistory));
@@ -252,6 +284,7 @@ function sendToLocalStorage(arr) {
 // write function which dynamically displays search history
 // ===========================================================================
 function appendHistory() {
+  // loop through searchHistory array if it is not empty and append to DOM
   if (searchHistory !== null) {
     for (i = 0; i < searchHistory.length; i++) {
       var listItem = document.createElement("button");
@@ -265,7 +298,7 @@ function appendHistory() {
 }
 
 // create event delegation listener for search history button which displays
-// forcast for clicked city
+// forecast for clicked city
 ul.addEventListener("click", appendCityToCurrent);
 
 // create an event listener for submit button
